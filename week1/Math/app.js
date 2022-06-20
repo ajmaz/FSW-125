752043c54e880d9215a2b474144892e7c0491e00
ajmaz <adam.mazloum@bryanuniversity.edu>
2022-06-20 02:40:41 -0400

    week1 math assignment

---
+++ week1/Math/app.js
@@ -0,0 +1,9 @@
+
+
+const mathFunc = require('./math')
+
+console.log(mathFunc)
+console.log(mathFunc.sum(10 , 10));
+console.log(mathFunc.subtract(10 , 5));
+console.log(mathFunc.multiply(10 , 2));
+console.log(mathFunc.divide(10 , 2));
